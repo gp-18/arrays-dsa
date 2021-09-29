4355b91cc7d594bd38241c413fcde64903b9d7f0
gp-18 <parthgupta1803@gmail.com>
2021-09-29 11:10:43 +0530

    adding string file

---
+++ udmey/03_strings.c
@@ -0,0 +1,74 @@
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+// char ch[]="parth gupta";
+// char ch1[20];
+// char ch2[]="op";
+
+// gets(ch1);
+// printf("%s",ch1);
+// printf("%s",ch);
+
+// int i=0;
+// while(ch2[i] !='\0')
+// {
+// printf("%s",ch2);
+// }
+// return 0;
+// }
+
+// #include<stdio.h>  
+// void main ()  
+// {  
+//     char s[11] = "javatpoint";  
+//     int i = 0;   
+//     int count = 0;  
+//     while(i<11)  
+//     {  
+//         if(s[i]=='a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'u' || s[i] == 'o')  
+//         {  
+//             count ++;  
+//         }  
+//         i++;  
+//     }  
+//     printf("The number of vowels %d",count);  
+// }  
+
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+// char ch[]="parth gupta";
+// char ch1[20];
+// printf("%d\n",strlen(ch));
+
+// strcpy(ch1,ch);
+// printf("%s\n",ch1);
+
+// strcat(ch1,ch);
+// printf("%s\n",ch);
+
+// strcmp(ch,ch1);
+
+// strrev(ch);
+// printf("%s\n",ch);
+
+// strlwr(ch);
+// printf("%s\n",ch);
+
+// strupr(ch);
+// printf("%s\n",ch);
+
+// printf("\n%f",ceil(3.6));    
+// printf("\n%f",ceil(3.3));    
+// printf("\n%f",floor(3.6));    
+// printf("\n%f",floor(3.2));    
+// printf("\n%f",sqrt(16));    
+// printf("\n%f",sqrt(7));    
+// printf("\n%f",pow(2,4));    
+// printf("\n%f",pow(3,3));    
+// printf("\n%d",abs(-12));     
+
+// return 0;
+// }
